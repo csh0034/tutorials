5bcf0e8d26869df4a0625fd8753ddf5ed2884512
csh0034 <csh0034@gmail.com>
2021-10-02 00:28:18 +0900

    feat spring core 프로젝트 세팅

---
+++ spring-core/src/main/java/com/ask/springcore/SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ask.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCoreApplication.class, args);
+  }
+
+}
---
+++ spring-core/src/test/java/com/ask/springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
