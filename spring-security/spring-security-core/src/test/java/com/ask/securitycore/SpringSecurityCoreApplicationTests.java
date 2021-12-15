f839907265051e790332d5c98349eed4580fabff
csh0034 <csh0034@gmail.com>
2021-12-15 14:36:16 +0900

    feat(spring-security-core): security test

--- spring-security/spring-security-core/src/test/java/com/ask/securitycore/SpringSecurityCoreApplicationTests.java
+++ spring-security/spring-security-core/src/test/java/com/ask/securitycore/SpringSecurityCoreApplicationTests.java
@@ -1,13 +1,41 @@
 package com.ask.securitycore;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringSecurityCoreApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Test
+  void authenticate() throws Exception {
+    // given
+    String id = "ask";
+    String password = "ask111";
+
+    // when
+    ResultActions result = mockMvc.perform(formLogin()
+        .loginProcessingUrl("/sub/login")
+        .user(id)
+        .password(password));
+
+    // then
+    result.andDo(print())
+        .andExpect(status().is3xxRedirection())
+        .andExpect(authenticated()
+            .withUsername("ask")
+            .withRoles("USER"));
+  }
 }
