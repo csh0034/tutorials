10922fc4123018abc28caece55e8970d0df380ab
csh0034 <csh0034@gmail.com>
2022-01-21 17:49:11 +0900

    feat(spring-swagger): init

---
+++ spring-swagger/README.md
@@ -0,0 +1,6 @@
+# Spring Swagger
+
+## 참조
+- [springfox swagger](https://springfox.github.io/springfox/docs/current/)
+- [github, swagger ui](https://github.com/swagger-api/swagger-ui)
+- [swagger 공식 홈페이지](https://swagger.io/tools/swagger-ui/)
---
+++ spring-swagger/src/test/java/com/ask/springswagger/SpringSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.springswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
