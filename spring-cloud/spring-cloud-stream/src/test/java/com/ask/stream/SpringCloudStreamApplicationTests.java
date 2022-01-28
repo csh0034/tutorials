474efb5a1332371a22e5f9d58e9f0bafddcb9b1c
csh0034 <csh0034@gmail.com>
2022-01-28 15:02:49 +0900

    feat(spring-cloud-stream): init

---
+++ spring-cloud/spring-cloud-stream/src/test/java/com/ask/stream/SpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
