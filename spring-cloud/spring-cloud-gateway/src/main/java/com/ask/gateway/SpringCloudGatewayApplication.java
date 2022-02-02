a45a30f1b74fa6759074cf47221a70e38f85ab59
csh0034 <csh0034@gmail.com>
2022-02-02 23:08:45 +0900

    feat(spring-cloud-gateway): init

---
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/ask/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ask.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudGatewayApplication.class, args);
+  }
+
+}
