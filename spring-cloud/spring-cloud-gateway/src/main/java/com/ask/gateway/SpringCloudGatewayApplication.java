9a049fbaa26a99bbf997be83e3accf3900f79a50
csh0034 <csh0034@gmail.com>
2022-02-04 02:55:17 +0900

    feat(spring-cloud-gateway): sleuth 적용 및 netty logging

--- spring-cloud/spring-cloud-gateway/src/main/java/com/ask/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/ask/gateway/SpringCloudGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCloudGatewayApplication {
 
   public static void main(String[] args) {
+    System.setProperty("reactor.netty.http.server.accessLogEnabled", "true");
     SpringApplication.run(SpringCloudGatewayApplication.class, args);
   }
 }
