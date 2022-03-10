3d3694e8a215627d259ae76dd88430e29b5cd223
csh0034 <csh0034@gmail.com>
2022-03-10 19:07:51 +0900

    refactor(spring-mail): 메일 전송 테스트

--- spring-mail/src/test/java/com/ask/springmail/SpringMailApplicationTests.java
+++ spring-mail/src/test/java/com/ask/springmail/SpringMailApplicationTests.java
@@ -1,13 +1,26 @@
 package com.ask.springmail;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class SpringMailApplicationTests {
+  
+  @Autowired
+  private JavaMailSender mailSender;
 
   @Test
-  void contextLoads() {
+  void mailSend() {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(String.format("SENDER<%s>", "no-reply@remotemeeting.com"));
+    message.setTo("shchoi1@rsupport.com");
+    message.setSubject("subject...");
+    message.setText("body...");
+
+    mailSender.send(message);
   }
 
 }
