675334d6d0dd71349749b5a51ae8ef4adbe13645
csh0034 <csh0034@gmail.com>
2022-03-17 22:51:30 +0900

    test(spring-jpa-querydsl): db 에 존재하지않는 entity 일 경우 검증

--- spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/EntityTest.java
+++ spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/EntityTest.java
@@ -1,5 +1,7 @@
 package com.ask.springjpaquerydsl;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 import com.ask.springjpaquerydsl.entity.Company;
 import com.ask.springjpaquerydsl.entity.User;
 import com.ask.springjpaquerydsl.repository.CompanyRepository;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @SpringBootTest
 @Slf4j
@@ -35,6 +38,19 @@ class EntityTest {
 
     // then
     log.info("createUser : {}", createdUser);
+  }
+
+  @DisplayName("db 에 존재하지않는 id의 entity 로 연관관계 대상과 persist 되는지 검증")
+  @Test
+  void saveWithDetachEntityOccurException() {
+    // given
+    Company detachCompany = createDetachCompany("not-exists-id");
+
+    // when then
+    User createdUser = User.create("abc123", "1234", detachCompany);
+
+    assertThatExceptionOfType(DataIntegrityViolationException.class)
+        .isThrownBy(() -> userRepository.save(createdUser));
   }
 
   @DisplayName("id 만 있는 detach 상태 entity 저장 검증")
