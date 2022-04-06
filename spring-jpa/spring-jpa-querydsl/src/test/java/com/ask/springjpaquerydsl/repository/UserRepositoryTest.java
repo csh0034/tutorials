7e2a5ce39bb3c8f9267e5bd44f677452f70e5415
csh0034 <csh0034@gmail.com>
2022-04-06 16:07:36 +0900

    feat(spring-jpa-querydsl): querydsl 5.0.0, paging

--- spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/repository/UserRepositoryTest.java
+++ spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/repository/UserRepositoryTest.java
@@ -1,18 +1,27 @@
 package com.ask.springjpaquerydsl.repository;
 
-import static com.ask.springjpaquerydsl.entity.QUser.user;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.ask.springjpaquerydsl.entity.QUser.*;
+import static org.assertj.core.api.Assertions.*;
 
-import com.ask.springjpaquerydsl.config.JpaConfig;
-import com.ask.springjpaquerydsl.entity.Company;
-import com.ask.springjpaquerydsl.entity.User;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.Objects;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.ask.springjpaquerydsl.config.JpaConfig;
+import com.ask.springjpaquerydsl.entity.Company;
+import com.ask.springjpaquerydsl.entity.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.extern.slf4j.Slf4j;
 
 @DataJpaTest
 @Import(JpaConfig.class)
@@ -47,4 +56,31 @@ class UserRepositoryTest {
     log.info("selectUser : {}", selectUser);
     assertThat(selectUser).isEqualTo(createUser);
   }
+
+  @DisplayName("paging 처리")
+  @Test
+  void paging() {
+    // given
+    Pageable pageable = PageRequest.of(3, 2);
+
+    // when
+    List<User> users = queryFactory.selectFrom(user)
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    Long total = queryFactory.select(user.count())
+        .from(user)
+        .fetchOne();
+
+    // then
+    Page<User> page = new PageImpl<>(users, pageable, total);
+
+    log.info("content: {}", page.getContent());
+    log.info("size: {}", page.getSize());
+    log.info("totalElements: {}", page.getTotalElements());
+    log.info("totalPages: {}", page.getTotalPages());
+    log.info("number: {}", page.getNumber());
+  }
+
 }
