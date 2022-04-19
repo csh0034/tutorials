fc8e14a258567d648021fc3b13d136a011194a9f
csh0034 <csh0034@gmail.com>
2022-04-19 16:52:21 +0900

    test(spring-jpa-querydsl): join alias 미지정 예외 검증

--- spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/repository/CompanyRepositoryTest.java
+++ spring-jpa/spring-jpa-querydsl/src/test/java/com/ask/springjpaquerydsl/repository/CompanyRepositoryTest.java
@@ -2,6 +2,7 @@ package com.ask.springjpaquerydsl.repository;
 
 import static com.ask.springjpaquerydsl.entity.QCompany.company;
 import static com.ask.springjpaquerydsl.entity.QUser.user;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.ask.springjpaquerydsl.config.JpaConfig;
 import com.querydsl.core.types.Projections;
@@ -10,6 +11,8 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.hql.internal.ast.QuerySyntaxException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -35,6 +38,23 @@ class CompanyRepositoryTest {
         .fetch();
 
     users.forEach(userDto -> log.info("userDto : {}", userDto));
+  }
+
+  @DisplayName("join alias 미지정시에 에외 발생 검증")
+  @Test
+  void withoutJoinAlias() {
+    // 하단과 같이 alias 사용하지 않을 경우 user.id, user.name 부분에서 Invalid path: 'user.id' 예외 발생
+    assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> queryFactory
+            .select(Projections.constructor(UserDto.class,
+                company.id,
+                company.name,
+                user.id,
+                user.name))
+            .from(company)
+            .leftJoin(company.users)
+            .fetch())
+        .withCauseInstanceOf(QuerySyntaxException.class)
+        .withMessageContaining("Invalid path");
   }
 
   @AllArgsConstructor
