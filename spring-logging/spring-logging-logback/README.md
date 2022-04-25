90b6da820f4b38317a2cce227380fd36435659b7
csh0034 <csh0034@gmail.com>
2022-04-25 14:53:20 +0900

    feat(spring-logging-logback): init

---
+++ spring-logging/spring-logging-logback/README.md
@@ -0,0 +1,6 @@
+# Spring Logging Logback
+
+## 참조
+
+- [Logback, Docs](https://logback.qos.ch/index.html)
+- [Mask Sensitive Data in Logs With Logback, baeldung](https://www.baeldung.com/logback-mask-sensitive-data)
---
+++ spring-logging/spring-logging-logback/src/main/java/com/ask/logback/SpringLoggingLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.ask.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggingLogbackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringLoggingLogbackApplication.class, args);
+  }
+
+}
