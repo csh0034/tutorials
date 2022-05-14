c413fd0818df6972d4643fe2c7a562393eed1701
csh0034 <csh0034@gmail.com>
2022-05-14 20:22:06 +0900

    feat(spring-jasypt): init

---
+++ spring-jasypt/README.md
@@ -0,0 +1,5 @@
+# Spring Jasypt
+
+## 참조
+
+- [GitHub, jasypt-spring-boot](https://github.com/ulisesbocchio/jasypt-spring-boot)
---
+++ spring-jasypt/src/main/java/com/ask/springjasypt/SpringJasyptApplication.java
@@ -0,0 +1,13 @@
+package com.ask.springjasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJasyptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJasyptApplication.class, args);
+  }
+
+}
