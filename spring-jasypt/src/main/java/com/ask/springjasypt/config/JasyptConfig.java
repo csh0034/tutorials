e350fa1e59e3122c157e2ccba20ae9d660eb089a
csh0034 <csh0034@gmail.com>
2022-05-15 18:34:53 +0900

    chore(spring-jasypt): 테스트코드 StringEncryptor bean 주입 수정

--- spring-jasypt/src/main/java/com/ask/springjasypt/config/JasyptConfig.java
+++ spring-jasypt/src/main/java/com/ask/springjasypt/config/JasyptConfig.java
@@ -30,12 +30,13 @@ public class JasyptConfig {
 //  public StringEncryptor jasyptStringEncryptor() {
 //    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 //    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//    config.setPassword("password");
 //    config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
-//    config.setPassword("asdf");
 //    config.setKeyObtentionIterations("1000");
 //    config.setPoolSize("1");
 //    config.setProviderName("SunJCE");
 //    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//    config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
 //    config.setStringOutputType("base64");
 //    encryptor.setConfig(config);
 //    return encryptor;
--- spring-jasypt/src/test/java/com/ask/springjasypt/JasyptTest.java
+++ spring-jasypt/src/test/java/com/ask/springjasypt/JasyptTest.java
@@ -1,16 +1,16 @@
 package com.ask.springjasypt;
 
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 class JasyptTest {
 
-  @Autowired
+  @Resource(name = "jasyptStringEncryptor")
   private StringEncryptor stringEncryptor;
 
   @Test
