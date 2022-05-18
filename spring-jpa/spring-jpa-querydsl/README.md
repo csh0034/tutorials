bc3c00923a4716c55b58a17ce682b29a52de2dff
csh0034 <csh0034@gmail.com>
2022-05-18 12:53:41 +0900

    feat(spring-jpa-querydsl): QClass deadlock 방지

--- spring-jpa/spring-jpa-querydsl/README.md
+++ spring-jpa/spring-jpa-querydsl/README.md
@@ -37,6 +37,17 @@
 </plugins>
 ```
 
+## querydsl deadlock 방지
+
+- Multithreaded initialization of Querydsl Q-types
+- QClass 가 멀티쓰레드에서 초기화 될때 순환 종속성이 존재할경우 deadlock 이 발생할 수 있다.
+- 하단 코드를 추가하여 여러 쓰레드에서 사용되기 전에 싱글쓰레드에서 미리 초기화를 시킬수 있다. 
+
+```java
+ClassPathUtils.scanPackage(Thread.currentThread().getContextClassLoader(), DOMAIN_PACKAGE);
+```
+
 ## 참조
 
-- [querydsl, GitHub](https://github.com/querydsl/querydsl/tree/master/querydsl-jpa)
+- [GitHub, querydsl](https://github.com/querydsl/querydsl/tree/master/querydsl-jpa)
+- [Reference, querydsl](http://querydsl.com/static/querydsl/5.0.0/reference/html_single/)
--- spring-jpa/spring-jpa-querydsl/src/main/java/com/ask/springjpaquerydsl/config/JpaConfig.java
+++ spring-jpa/spring-jpa-querydsl/src/main/java/com/ask/springjpaquerydsl/config/JpaConfig.java
@@ -1,15 +1,25 @@
 package com.ask.springjpaquerydsl.config;
 
+import com.querydsl.codegen.ClassPathUtils;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JpaConfig {
+public class JpaConfig implements InitializingBean {
+
+  private static final String DOMAIN_PACKAGE = "com.ask.springjpaquerydsl.entity";
 
   @Bean
   public JPAQueryFactory queryFactory(EntityManager entityManager) {
     return new JPAQueryFactory(entityManager);
   }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    ClassPathUtils.scanPackage(Thread.currentThread().getContextClassLoader(), DOMAIN_PACKAGE);
+  }
+
 }
