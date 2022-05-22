145e14793637ef745430376b1e85b0f299bf49b3
csh0034 <csh0034@gmail.com>
2022-05-22 13:33:59 +0900

    feat(spring-lock-shedlock): init

---
+++ spring-lock/spring-lock-shedlock/README.md
@@ -0,0 +1,5 @@
+# Spring ShedLock
+
+## 참조
+
+- [GitHub, ShedLock](https://github.com/lukas-krecan/ShedLock#redis-using-spring-redisconnectionfactory)
---
+++ spring-lock/spring-lock-shedlock/src/main/java/com/ask/shedlock/SpringLockShedlockApplication.java
@@ -0,0 +1,13 @@
+package com.ask.shedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLockShedlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLockShedlockApplication.class, args);
+	}
+
+}
