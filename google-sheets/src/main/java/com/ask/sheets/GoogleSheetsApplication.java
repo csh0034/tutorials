42dbbbba1f63252b29afde4d36b013ac53586fe0
csh0034 <csh0034@gmail.com>
2022-05-23 21:51:10 +0900

    feat(google-sheets): init

---
+++ google-sheets/src/main/java/com/ask/sheets/GoogleSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.ask.sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSheetsApplication.class, args);
+	}
+
+}
