b8fff7650f0cf8b69be8ace941463d13365d6c56
csh0034 <csh0034@gmail.com>
2022-05-30 19:25:46 +0900

    chore(google-api): 인덴트 수정

--- google-api/google-sheets/src/main/java/com/ask/sheets/GoogleSheetsApplication.java
+++ google-api/google-sheets/src/main/java/com/ask/sheets/GoogleSheetsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoogleSheetsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoogleSheetsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleSheetsApplication.class, args);
+  }
 
 }
