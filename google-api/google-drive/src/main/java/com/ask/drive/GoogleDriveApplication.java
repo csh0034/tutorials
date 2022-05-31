81253bc045fc0502c1a7c6eb3914a4313650da96
csh0034 <csh0034@gmail.com>
2022-05-31 20:11:09 +0900

    feat(google-drive): init

---
+++ google-api/google-drive/src/main/java/com/ask/drive/GoogleDriveApplication.java
@@ -0,0 +1,13 @@
+package com.ask.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleDriveApplication.class, args);
+  }
+
+}
---
+++ google-api/google-drive/src/test/java/com/ask/drive/GoogleDriveApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ask.drive;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class GoogleDriveApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
