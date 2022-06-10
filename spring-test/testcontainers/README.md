c9454834dcbb4562f23464080cf88e83041ac8c2
csh0034 <csh0034@gmail.com>
2022-06-10 14:21:47 +0900

    feat(testcontainers): init

---
+++ spring-test/testcontainers/README.md
@@ -0,0 +1,5 @@
+# Testcontainers
+
+## 참조
+
+- [Reference, Testcontainers](https://www.testcontainers.org/)
---
+++ spring-test/testcontainers/src/main/java/com/ask/testcontainers/TestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.ask.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestcontainersApplication.class, args);
+  }
+
+}
