67d0baa3edcb3e5864f951a462b8264d0784a6a2
csh0034 <csh0034@gmail.com>
2022-06-11 21:24:53 +0900

    refactor(spring-dynamodb): localstack 사용하도록 Credential 변경

--- spring-dynamodb/src/main/java/com/ask/springdynamodb/dynamo/DynamoConfig.java
+++ spring-dynamodb/src/main/java/com/ask/springdynamodb/dynamo/DynamoConfig.java
@@ -3,7 +3,8 @@ package com.ask.springdynamodb.dynamo;
 import java.net.URI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -14,8 +15,8 @@ public class DynamoConfig {
   @Bean
   public DynamoDbEnhancedClient enhancedClient() {
     DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
-        .credentialsProvider(ProfileCredentialsProvider.create())
-        .endpointOverride(URI.create("http://localhost:8000"))
+        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
+        .endpointOverride(URI.create("http://localhost:4566"))
         .region(Region.AP_NORTHEAST_2)
         .build();
 
