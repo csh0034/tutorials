6984895d3c53cfe3186ee0a584ce967a256d0748
csh0034 <csh0034@gmail.com>
2022-06-11 20:03:14 +0900

    feat(localstack): init

---
+++ localstack/src/main/java/com/ask/localstack/LocalstackApplication.java
@@ -0,0 +1,13 @@
+package com.ask.localstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LocalstackApplication.class, args);
+  }
+
+}
---
+++ localstack/src/test/java/com/ask/localstack/LocalstackApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ask.localstack;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class LocalstackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
