ad777fa216ea0c6b66faeb5cf93af67153fa0fe0
csh0034 <csh0034@gmail.com>
2022-06-13 11:48:56 +0900

    test(spring-cloud-aws-parameter-store): 프로퍼티 주입 검증

--- spring-cloud-aws/spring-cloud-aws-parameter-store/src/test/java/com/ask/awsparameterstore/SpringCloudAwsParameterStoreApplicationTests.java
+++ spring-cloud-aws/spring-cloud-aws-parameter-store/src/test/java/com/ask/awsparameterstore/SpringCloudAwsParameterStoreApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ask.awsparameterstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ask.awsparameterstore.config.CustomProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudAwsParameterStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private CustomProperties customProperties;
+
+  @Test
+  void validate() {
+    assertThat(customProperties).usingRecursiveComparison().isEqualTo(new CustomProperties("ASk", "1234"));
+  }
 
 }
