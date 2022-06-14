964824adf770113cf92d00de69950dd3c72b1055
csh0034 <csh0034@gmail.com>
2022-06-14 10:03:56 +0900

    feat(spring-cloud-aws-secrets-manager): localstack 사용하여 프로퍼티 세팅

--- spring-cloud-aws/spring-cloud-aws-secrets-manager/src/main/java/com/ask/awssecretsmanager/SpringCloudAwsSecretsManagerApplication.java
+++ spring-cloud-aws/spring-cloud-aws-secrets-manager/src/main/java/com/ask/awssecretsmanager/SpringCloudAwsSecretsManagerApplication.java
@@ -1,13 +1,25 @@
 package com.ask.awssecretsmanager;
 
+import com.ask.awssecretsmanager.config.CustomProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@Slf4j
 public class SpringCloudAwsSecretsManagerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringCloudAwsSecretsManagerApplication.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner applicationRunner(CustomProperties customProperties) {
+    return args -> log.info("customProperties: {}", customProperties);
   }
 
 }
--- spring-cloud-aws/spring-cloud-aws-secrets-manager/src/test/java/com/ask/awssecretsmanager/SpringCloudAwsSecretsManagerApplicationTests.java
+++ spring-cloud-aws/spring-cloud-aws-secrets-manager/src/test/java/com/ask/awssecretsmanager/SpringCloudAwsSecretsManagerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ask.awssecretsmanager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ask.awssecretsmanager.config.CustomProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudAwsSecretsManagerApplicationTests {
 
+  @Autowired
+  private CustomProperties customProperties;
+
   @Test
-  void contextLoads() {
+  void validate() {
+    assertThat(customProperties).usingRecursiveComparison().isEqualTo(new CustomProperties("ASk", "1234"));
   }
+
 
 }
