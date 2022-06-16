36380f975d9118135e265fae6309d60b586ae0d7
csh0034 <csh0034@gmail.com>
2022-06-16 17:35:04 +0900

    feat(spring-core): pathVariable Optional

--- spring-core/src/main/java/com/ask/springcore/mapping/MappingController.java
+++ spring-core/src/main/java/com/ask/springcore/mapping/MappingController.java
@@ -1,5 +1,6 @@
 package com.ask.springcore.mapping;
 
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,14 @@ public class MappingController {
   })
   public String mapping(@PathVariable(required = false) String locale) {
     return String.format("locale: %s", locale);
+  }
+
+  @GetMapping({
+      "/mapping2",
+      "/mapping2/{locale:ko|en}"
+  })
+  public String mapping2(@PathVariable Optional<String> locale) {
+    return String.format("locale: %s", locale.orElse("default"));
   }
 
 }
