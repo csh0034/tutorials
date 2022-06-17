e0a1acf416e7e7080e4e32129361772a4e2fdf9a
csh0034 <csh0034@gmail.com>
2022-06-17 15:05:56 +0900

    feat(spring-async): init

---
+++ spring-async/src/main/java/com/ask/springasync/SpringAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.ask.springasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAsyncApplication.class, args);
+  }
+
+}
---
+++ spring-async/src/test/java/com/ask/springasync/SpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.springasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsyncApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
