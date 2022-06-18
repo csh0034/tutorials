f6290bc5f6c6e81492709deb86a48260870e967d
csh0034 <csh0034@gmail.com>
2022-06-18 16:58:47 +0900

    feat(spring-cloud-aws-messaging-sns): init

---
+++ spring-cloud-aws/spring-cloud-aws-messaging-sns/src/main/java/com/ask/awsmessagingsns/SpringCloudAwsMessagingSnsApplication.java
@@ -0,0 +1,13 @@
+package com.ask.awsmessagingsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAwsMessagingSnsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudAwsMessagingSnsApplication.class, args);
+  }
+
+}
---
+++ spring-cloud-aws/spring-cloud-aws-messaging-sns/src/test/java/com/ask/awsmessagingsns/SpringCloudAwsMessagingSnsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ask.awsmessagingsns;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class SpringCloudAwsMessagingSnsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
