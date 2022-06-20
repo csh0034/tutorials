0868a374736c5266d5d64bed6474808ea9aa76e8
csh0034 <csh0034@gmail.com>
2022-06-20 13:28:17 +0900

    feat(spring-test-mockito): init

---
+++ spring-test/spring-test-mockito/src/test/java/com/ask/mockito/SpringTestMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestMockitoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
