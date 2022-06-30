6dd579b06e1df7026ac58c28214915ff26463305
csh0034 <csh0034@gmail.com>
2022-06-30 09:32:40 +0900

    feat(resource-server): AuthenticationPrincipal, Principal

--- spring-security/resource-server/src/main/java/com/ask/resourceserver/web/OAuth2ResourceServerController.java
+++ spring-security/resource-server/src/main/java/com/ask/resourceserver/web/OAuth2ResourceServerController.java
@@ -1,5 +1,7 @@
 package com.ask.resourceserver.web;
 
+import java.security.Principal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class OAuth2ResourceServerController {
 
+  /**
+   * @param jwt AuthenticationPrincipalArgumentResolver 에서 처리
+   * @param principal ServletRequestMethodArgumentResolver 에서 처리
+   */
   @GetMapping("/")
-  public String index(@AuthenticationPrincipal Jwt jwt) {
+  public String index(@AuthenticationPrincipal Jwt jwt, Principal principal) {
+    log.info("jwt: {}", jwt);
+    log.info("principal: {}", principal);
+    log.info("principal.name: {}", principal.getName());
     return String.format("Hello, %s!", jwt.getSubject());
   }
 
