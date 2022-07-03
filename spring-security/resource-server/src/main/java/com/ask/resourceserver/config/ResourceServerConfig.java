13bcce21ada9bb6d052aecddbbf1eed0332cb3f7
csh0034 <csh0034@gmail.com>
2022-07-03 23:04:55 +0900

    refactor(resource-server): SecurityFilterChain Bean 방식 적용

--- spring-security/resource-server/src/main/java/com/ask/resourceserver/config/ResourceServerConfig.java
+++ spring-security/resource-server/src/main/java/com/ask/resourceserver/config/ResourceServerConfig.java
@@ -1,26 +1,24 @@
 package com.ask.resourceserver.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors(Customizer.withDefaults())
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -28,7 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.GET, "/message/**").hasAuthority("SCOPE_message:read")
             .antMatchers(HttpMethod.POST, "/message/**").hasAuthority("SCOPE_message:write")
             .anyRequest().authenticated())
-        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .build();
   }
 
   @Bean
