45d4b8ac5b7ec3c0ecb061d67c035aebcec09f0b
csh0034 <csh0034@gmail.com>
2022-07-03 22:08:41 +0900

    test(resource-server): 만료시간이 지난 jwt 예외

--- spring-security/resource-server/src/test/java/com/ask/resourceserver/ResourceServerApplicationTests.java
+++ spring-security/resource-server/src/test/java/com/ask/resourceserver/ResourceServerApplicationTests.java
@@ -7,39 +7,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+import java.util.Properties;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("classpath:token.properties")
 class ResourceServerApplicationTests {
 
-  @Value("${noScopesToken}")
-  private String noScopesToken;
+  private static final String NO_SCOPES_TOKEN;
+  private static final String MESSAGE_READ_TOKEN;
+  private static final String EXPIRED_TOKEN;
 
-  @Value("${messageReadToken}")
-  private String messageReadToken;
+  static {
+    try {
+      Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("token.properties"));
+
+      NO_SCOPES_TOKEN = properties.getProperty("noScopesToken");
+      MESSAGE_READ_TOKEN = properties.getProperty("messageReadToken");
+      EXPIRED_TOKEN = properties.getProperty("expiredToken");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Autowired
   private MockMvc mvc;
 
-  @DisplayName("/ : 스코프가 없는 토큰으로 접근 가능하다.")
+  @DisplayName("/ : 스코프가 없는 토큰으로 접근 가능")
   @Test
   void validTokenNoScope() throws Exception {
     // when
     ResultActions result = mvc.perform(get("/")
-        .with(bearerToken(noScopesToken)));
+        .with(bearerToken(NO_SCOPES_TOKEN)));
 
     // then
     result.andDo(print())
@@ -47,12 +58,12 @@ class ResourceServerApplicationTests {
         .andExpect(content().string(containsString("Hello, messaging-client!")));
   }
 
-  @DisplayName("/message : Read 토큰으로 접근 가능하다.")
+  @DisplayName("/message : Read 토큰으로 접근 가능")
   @Test
   void validTokenReadScope() throws Exception {
     // when
     ResultActions result = mvc.perform(get("/message")
-        .with(bearerToken(messageReadToken)));
+        .with(bearerToken(MESSAGE_READ_TOKEN)));
 
     // then
     result.andDo(print())
@@ -60,17 +71,31 @@ class ResourceServerApplicationTests {
         .andExpect(content().string(containsString("secret message")));
   }
 
-  @DisplayName("/message : 스코프가 없는 토큰으로 접근 불가능하다.")
+  @DisplayName("/message : 스코프가 없는 토큰으로 접근 불가능")
   @Test
   void invalidTokenNoScope() throws Exception {
     // when
     ResultActions result = mvc.perform(get("/message")
-        .with(bearerToken(noScopesToken)));
+        .with(bearerToken(NO_SCOPES_TOKEN)));
 
     // then
     result.andDo(print())
         .andExpect(status().isForbidden())
         .andExpect(header().string(HttpHeaders.WWW_AUTHENTICATE, containsString("Bearer error=\"insufficient_scope\"")));
+  }
+
+  @DisplayName("만료시간이 다되었을경우 접근 불가능")
+  @Test
+  void invalidTokenExpired() throws Exception {
+    // when
+    ResultActions result = mvc.perform(get("/message")
+        .with(bearerToken(EXPIRED_TOKEN)));
+
+    // then
+    result.andDo(print())
+        .andExpect(status().isUnauthorized())
+        .andExpect(header().string(HttpHeaders.WWW_AUTHENTICATE, containsString("Bearer error=\"invalid_token\"")))
+        .andExpect(header().string(HttpHeaders.WWW_AUTHENTICATE, containsString("Jwt expired at")));
   }
 
   private static BearerTokenRequestPostProcessor bearerToken(String token) {
@@ -91,4 +116,5 @@ class ResourceServerApplicationTests {
       return request;
     }
   }
+
 }
