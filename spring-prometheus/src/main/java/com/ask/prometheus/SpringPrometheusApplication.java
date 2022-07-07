f5bfeb6aee97ff6c64fc20f0800efb75d2bdb57a
csh0034 <csh0034@gmail.com>
2022-07-07 09:31:08 +0900

    feat(spring-prometheus): init

---
+++ spring-prometheus/src/main/java/com/ask/prometheus/SpringPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.ask.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrometheusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringPrometheusApplication.class, args);
+  }
+
+}
---
+++ spring-prometheus/src/test/java/com/ask/prometheus/SpringPrometheusApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ask.prometheus;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class SpringPrometheusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
