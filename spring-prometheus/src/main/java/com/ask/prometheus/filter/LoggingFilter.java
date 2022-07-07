e1d8ae135c5c5f3e496e38dcc52ea4e8f7091a3b
csh0034 <csh0034@gmail.com>
2022-07-07 11:24:21 +0900

    feat(spring-prometheus): actuator, jpa 설정

---
+++ spring-prometheus/src/main/java/com/ask/prometheus/filter/LoggingFilter.java
@@ -0,0 +1,32 @@
+package com.ask.prometheus.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.filter.OrderedFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LoggingFilter implements OrderedFilter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    log.info("[{}] {}", request.getMethod(), request.getRequestURI());
+
+    chain.doFilter(servletRequest, servletResponse);
+  }
+
+  @Override
+  public int getOrder() {
+    return Ordered.HIGHEST_PRECEDENCE;
+  }
+}
+
