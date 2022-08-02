603840f0dfa45f4fbe41ac809c67745f093ac9ec
csh0034 <csh0034@gmail.com>
2022-08-02 16:39:51 +0900

    feat(google-api-client): init

---
+++ google-api/google-api-client/src/main/java/com/ask/googleapiclient/GoogleApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.ask.googleapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApiClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleApiClientApplication.class, args);
+  }
+
+}
---
+++ google-api/google-api-client/src/test/java/com/ask/googleapiclient/GoogleApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.googleapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApiClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
