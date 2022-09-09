b77316749eb9b4873d108d5642e658da1b414a77
csh0034 <csh0034@gmail.com>
2022-09-09 21:35:39 +0900

    feat(kitchenpos): init

---
+++ ddd/kitchenpos/src/main/java/com/ask/kitchenpos/KitchenPosApplication.java
@@ -0,0 +1,13 @@
+package com.ask.kitchenpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenPosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KitchenPosApplication.class, args);
+  }
+
+}
