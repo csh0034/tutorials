bc7fcd881956773718912ff2ded0e0f8ac69374a
csh0034 <csh0034@gmail.com>
2022-10-02 21:11:51 +0900

    feat(spring-core): add ApplicationConversionService

---
+++ spring-core/src/main/java/com/ask/springcore/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ask.springcore.config;
+
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    ApplicationConversionService.configure(registry);
+  }
+
+}
