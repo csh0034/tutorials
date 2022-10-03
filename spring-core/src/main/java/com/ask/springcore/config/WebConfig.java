bb33d3e73fd80f1b40564088b512458065850388
csh0034 <csh0034@gmail.com>
2022-10-03 14:01:11 +0900

    chore(spring-core): 주석추가

--- spring-core/src/main/java/com/ask/springcore/config/WebConfig.java
+++ spring-core/src/main/java/com/ask/springcore/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
+    // Converter 는 Bean 으로 등록만하면 자동으로 동작하지만 ConverterFactory 는 직접 등록해줘야함
     registry.addConverterFactory(new LenientStringToEnumConverterFactory());
 //    ApplicationConversionService.configure(registry);
   }
