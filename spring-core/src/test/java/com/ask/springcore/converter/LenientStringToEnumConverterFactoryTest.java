3f11fe212eb0a20ff91c649bd3e14c290ad22cfc
csh0034 <csh0034@gmail.com>
2022-10-11 21:59:25 +0900

    test(spring-core): LenientStringToEnumConverterFactoryTest

--- spring-core/src/test/java/com/ask/springcore/converter/LenientStringToEnumConverterFactoryTest.java
+++ spring-core/src/test/java/com/ask/springcore/converter/LenientStringToEnumConverterFactoryTest.java
@@ -3,77 +3,79 @@ package com.ask.springcore.converter;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import com.ask.springcore.config.converter.Code;
 import com.ask.springcore.config.converter.LenientStringToEnumConverterFactory;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.core.convert.converter.Converter;
 
-public class LenientStringToEnumConverterFactoryTest {
+class LenientStringToEnumConverterFactoryTest {
 
-  private LenientStringToEnumConverterFactory factory;
+  private Converter<String, TestType> converter;
 
   @BeforeEach
   void setUp() {
-    factory = new LenientStringToEnumConverterFactory();
+    LenientStringToEnumConverterFactory factory = new LenientStringToEnumConverterFactory();
+    converter = factory.getConverter(TestType.class);
   }
 
   @DisplayName("대소문자 구분안함, 특수문자 제거 처리후 비교")
   @ParameterizedTest
-  @ValueSource(strings = {"A_GOOGLE", "agoogle", "a_google", "a$google", "a^google"})
+  @ValueSource(strings = {"A_TYPE", "a_type", "atype", "a$type", "a^type"})
   void convert(String source) {
-    // given
-    Converter<String, Company> converter = factory.getConverter(Company.class);
-
     // when
-    Company company = converter.convert(source);
+    TestType result = converter.convert(source);
 
     // then
-    assertThat(company).isEqualTo(Company.A_GOOGLE);
+    assertThat(result).isSameAs(TestType.A_TYPE);
   }
 
-  @DisplayName("Code 인터페이스 상속한 Enum 일 경우 해당 Code 값으로 변경 가능")
-  @Test
-  void convert() {
+  @DisplayName("Code 인터페이스 구현한 Enum 일 경우 해당 Code 값으로 변경 가능")
+  @ParameterizedTest
+  @EnumSource(TestType.class)
+  void convert(TestType testType) {
     // given
-    Converter<String, Company> converter = factory.getConverter(Company.class);
-    String source = "1";
+    String code = testType.getCode();
 
     // when
-    Company company = converter.convert(source);
+    TestType result = converter.convert(code);
 
     // then
-    assertThat(company).isEqualTo(Company.A_GOOGLE);
+    assertThat(result).isSameAs(testType);
   }
 
-  /**
-   * null 일 경우엔 여기 까지 들어오지 않음
-   */
   @DisplayName("문자열이 비어있을 경우 null 반환")
   @ParameterizedTest
   @EmptySource
   void convertEmpty(String source) {
-    // given
-    Converter<String, Company> converter = factory.getConverter(Company.class);
-
     // when
-    Company company = converter.convert(source);
+    TestType result = converter.convert(source);
 
     // then
-    assertThat(company).isNull();
+    assertThat(result).isNull();
   }
 
   @DisplayName("Enum 으로 변환할수 없을 경우 예외 발생")
   @Test
   void convertFail() {
-    // given
-    Converter<String, Company> converter = factory.getConverter(Company.class);
-
     // when then
     assertThatIllegalArgumentException().isThrownBy(() -> converter.convert("invalid-source"));
+  }
+
+  @RequiredArgsConstructor
+  @Getter
+  enum TestType implements Code {
+    A_TYPE("1"),
+    B_TYPE("2");
+
+    private final String code;
   }
 
 }
