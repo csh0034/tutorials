0d935d7f3a54488e616530592fc1d69d379c2641
csh0034 <csh0034@gmail.com>
2022-10-28 14:40:30 +0900

    refactor(spring-jpa-core): 트랜잭션 aop 처리, type safe

--- spring-jpa/spring-jpa-core/src/main/java/com/ask/springjpacore/config/JpaConfig.java
+++ spring-jpa/spring-jpa-core/src/main/java/com/ask/springjpacore/config/JpaConfig.java
@@ -1,11 +1,11 @@
 package com.ask.springjpacore.config;
 
-import java.util.Properties;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 
@@ -13,22 +13,25 @@ import org.springframework.transaction.interceptor.TransactionInterceptor;
 public class JpaConfig {
 
   @Bean
-  public Advisor transactionAdviceAdvisor(TransactionManager transactionManager) {
-    Properties properties = new Properties();
-    properties.put("*", "PROPAGATION_REQUIRED");
-    properties.put("get*", "PROPAGATION_REQUIRED,readOnly");
-    properties.put("find*", "PROPAGATION_REQUIRED,readOnly");
-    properties.put("is*", "PROPAGATION_REQUIRED,readOnly");
-    properties.put("check*", "PROPAGATION_REQUIRED,readOnly");
-    properties.put("exists*", "PROPAGATION_REQUIRED,readOnly");
-
+  public Advisor transactionAdvisor(TransactionManager transactionManager) {
     NameMatchTransactionAttributeSource txSource = new NameMatchTransactionAttributeSource();
-    txSource.setProperties(properties);
+
+    txSource.addTransactionalMethod("*", new DefaultTransactionAttribute());
+    addReadOnlyTransactionalMethods(txSource, "get*", "find*", "is*", "check*", "exists*");
 
     AspectJExpressionPointcutAdvisor advisor = new AspectJExpressionPointcutAdvisor();
     advisor.setAdvice(new TransactionInterceptor(transactionManager, txSource));
     advisor.setExpression("execution(* com..*.*Service.*(..))");
     return advisor;
+  }
+
+  private void addReadOnlyTransactionalMethods(NameMatchTransactionAttributeSource txSource, String... methodNames) {
+    DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
+    transactionAttribute.setReadOnly(true);
+
+    for (String methodName : methodNames) {
+      txSource.addTransactionalMethod(methodName, transactionAttribute);
+    }
   }
 
 }
