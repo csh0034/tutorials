d0f0af2e83b96fbaffc05563b574ffacbc321fd0
csh0034 <csh0034@gmail.com>
2022-11-06 18:20:41 +0900

    feat(oauth2-client): keycloak logout 처리
    - 로그아웃처리시 OP(OpenID Connect 제공자) 에서도 로그아웃 처리

--- spring-security/oauth2-client/src/main/java/com/ask/oauth2client/config/SecurityConfig.java
+++ spring-security/oauth2-client/src/main/java/com/ask/oauth2client/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.ask.oauth2client.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final ClientRegistrationRepository clientRegistrationRepository;
 
   /**
    * @see org.springframework.security.config.oauth2.client.CommonOAuth2Provider
@@ -20,7 +26,14 @@ public class SecurityConfig {
             .antMatchers("/").permitAll()
             .anyRequest().authenticated())
         .oauth2Login(Customizer.withDefaults())
+        .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
         .build();
+  }
+
+  private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+    OidcClientInitiatedLogoutSuccessHandler handler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+    handler.setPostLogoutRedirectUri("{baseUrl}");
+    return handler;
   }
 
 }
