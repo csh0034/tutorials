9dddb10eadf143acf7dc50b7e15a64019edb58a2
csh0034 <csh0034@gmail.com>
2022-11-24 13:56:12 +0900

    test(apache-camel): EndpointRouteBuilder, ProducerTemplate, ConsumerTemplate

--- apache-camel/src/test/java/com/ask/apachecamel/ApacheCamelApplicationTests.java
+++ apache-camel/src/test/java/com/ask/apachecamel/ApacheCamelApplicationTests.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,12 @@ class ApacheCamelApplicationTests {
   @DisplayName("csv marshal")
   @Test
   void marshal() {
+    // Event Message (InOnly)
     producerTemplate.sendBody("direct:marshal", Fixtures.employees());
+
+    // request methods - Request Reply (InOut)
+    String result = producerTemplate.requestBody("direct:marshal", Fixtures.employees(), String.class);
+    log.info("result: {}", result);
   }
 
   @DisplayName("fixed length marshal")
@@ -47,20 +53,34 @@ class ApacheCamelApplicationTests {
     producerTemplate.sendBody("direct:unmarshal2", Fixtures.stringFixedLengthDto());
   }
 
+  @DisplayName("수동으로 메세지를 가져온다.")
+  @Test
+  void manualRoute() {
+    String endpointUri = "direct:manualRoute?timeout=5000";
+
+    // 동기로 보낼 경우 receive 할때 까지 blocking 상태이므로 비동기 send 처리함
+    producerTemplate.asyncSendBody(endpointUri, "sample!!");
+
+//    String result = (String) consumerTemplate.receiveBodyNoWait(endpointUri);
+    String result = (String) consumerTemplate.receiveBody(endpointUri, 2000);
+    log.info("result: {}", result);
+  }
 
   @TestConfiguration
   static class TestConfig {
 
     @Bean
-    public RouteBuilder marshalRoute() {
-      return new RouteBuilder() {
+    public EndpointRouteBuilder marshalRoute() {
+      return new EndpointRouteBuilder() {
         @Override
         public void configure() {
           from("direct:marshal")
 //              .bean("sampleBean")
               .to("bean:sampleBean")
 //              .marshal(dataFormat)
+//              .marshal().beanio("mapping.xml", "employeeFile")
 //              .to("dataformat:beanio:marshal?mapping=mapping.xml&streamName=employeeFile") // 자동설정에 의해 지정 안해도됨.
+//              .to(dataformat("beanio:marshal"))
               .to("dataformat:beanio:marshal")
               .process(exchange -> {
                 String body = exchange.getIn().getBody(String.class);
@@ -86,11 +106,12 @@ class ApacheCamelApplicationTests {
     }
 
     @Bean
-    public RouteBuilder unmarshalRoute() {
-      return new RouteBuilder() {
+    public EndpointRouteBuilder unmarshalRoute() {
+      return new EndpointRouteBuilder() {
         @Override
         public void configure() {
-          from("direct:unmarshal")
+//          from("direct:unmarshal")
+          from(direct("unmarshal"))
 //              .unmarshal(dataFormat)
               .to("dataformat:beanio:unmarshal")
               .split(body())
@@ -117,6 +138,17 @@ class ApacheCamelApplicationTests {
         }
       };
     }
+
+//    @Bean
+//    public EndpointRouteBuilder manualRoute() {
+//      return new EndpointRouteBuilder() {
+//        @Override
+//        public void configure() {
+//          from(direct("manualRoute"))
+//              .log("${body}");
+//        }
+//      };
+//    }
 
   }
 
