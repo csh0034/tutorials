58d2779b510b4498e75848c773ad014c8e9982e0
csh0034 <csh0034@gmail.com>
2022-12-06 19:25:04 +0900

    test(java-core): SimpleDateFormat timezone

---
+++ java-core/src/test/java/com/ask/javacore/DateTest.java
@@ -0,0 +1,29 @@
+package com.ask.javacore;
+
+import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+  /**
+   * 서버가 utc 라면 그대로 utc 로 내려가고 <br>
+   * kst 라면 utc 가 적용되어 -9 시간 하여 포맷팅된다.
+   */
+  @DisplayName("서버 타임존 상관없이 utc 로 formatting")
+  @Test
+  void timezone() {
+    Date date = new Date();
+    TimeZone utc = TimeZone.getTimeZone(ZoneOffset.UTC);
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    simpleDateFormat.setTimeZone(utc);
+
+    String result = simpleDateFormat.format(date);
+    System.out.println(result);
+  }
+
+}
