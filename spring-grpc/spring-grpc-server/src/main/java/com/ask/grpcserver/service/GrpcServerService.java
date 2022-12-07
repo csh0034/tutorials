2e38327ab18a07f166719e09990c99c7d83674da
csh0034 <csh0034@gmail.com>
2022-12-07 12:41:58 +0900

    chore(spring-grpc): port 및 출력 수정

--- spring-grpc/spring-grpc-server/src/main/java/com/ask/grpcserver/service/GrpcServerService.java
+++ spring-grpc/spring-grpc-server/src/main/java/com/ask/grpcserver/service/GrpcServerService.java
@@ -14,7 +14,7 @@ public class GrpcServerService extends GreeterGrpc.GreeterImplBase {
 
   @Override
   public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-    log.info("request, name: {}", request.getName());
+    log.info("[request] name: {}", request.getName());
 
     HelloReply reply = HelloReply.newBuilder()
         .setMessage("Hello: " + request.getName())
@@ -25,9 +25,9 @@ public class GrpcServerService extends GreeterGrpc.GreeterImplBase {
 
   @Override
   public void sayRepeatHello(RepeatHelloRequest request, StreamObserver<HelloReply> responseObserver) {
-    log.info("request, name: {} count: {} | ", request.getName(), request.getCount());
+    log.info("[request] name: {}, count: {}", request.getName(), request.getCount());
 
-    for (int i = 0; i < request.getCount(); i++) {
+    for (int i = 1; i < request.getCount() + 1; i++) {
       HelloReply reply = HelloReply.newBuilder()
           .setMessage(String.format("[%d/%d] Hello: %s", i, request.getCount(), request.getName()))
           .build();
