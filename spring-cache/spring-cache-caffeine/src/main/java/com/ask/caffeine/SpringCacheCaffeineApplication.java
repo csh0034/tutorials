58f9e27e13e03ffacca9d61834ad370905a57262
csh0034 <csh0034@gmail.com>
2022-12-20 18:25:37 +0900

    feat(spring-cache-caffeine): init

---
+++ spring-cache/spring-cache-caffeine/src/main/java/com/ask/caffeine/SpringCacheCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.ask.caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheCaffeineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCacheCaffeineApplication.class, args);
+  }
+
+}
---
+++ spring-cache/spring-cache-caffeine/src/test/java/com/ask/caffeine/SpringCacheCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.caffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheCaffeineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
