7136ca3e7c78e7b44445f31d436d3f11b432d3ce
csh0034 <csh0034@gmail.com>
2022-12-22 16:10:15 +0900

    test(spring-redis): ValueOperationsTest

--- spring-redis/src/test/java/com/ask/springredis/ValueOperationsTest.java
+++ spring-redis/src/test/java/com/ask/springredis/ValueOperationsTest.java
@@ -2,12 +2,17 @@ package com.ask.springredis;
 
 import java.time.Duration;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
+@Slf4j
 class ValueOperationsTest {
 
   private static final String KEY = "key";
@@ -32,6 +37,34 @@ class ValueOperationsTest {
   @Test
   void delete() {
     valueOperations.getOperations().delete(KEY);
+  }
+
+  @DisplayName("EXISTS, Available since: 1.0.0")
+  @Test
+  void hasKey() {
+    Boolean result = valueOperations.getOperations().hasKey(KEY);
+    log.info("result: {}", result);
+  }
+
+  @DisplayName("INCRBY, Available since: 1.0.0")
+  @Test
+  void increment() {
+    Long result = valueOperations.increment(KEY);
+    log.info("result: {}", result);
+  }
+
+  @DisplayName("SETEX + INCRBY")
+  @Test
+  void setAndIncrement() {
+    valueOperations.set(KEY, "1", Duration.ofMinutes(1));
+    Long result = valueOperations.increment(KEY);
+    log.info("result: {}", result);
+  }
+
+  @AfterAll
+  static void afterAll(@Autowired StringRedisTemplate stringRedisTemplate) {
+    log.info("called");
+    stringRedisTemplate.delete(KEY);
   }
 
 }
