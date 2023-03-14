c8ceea84a00362e2f070f8b24de530818d28751b
csh0034 <csh0034@gmail.com>
2023-03-14 14:57:03 +0900

    chore(spring-core): add comment

--- spring-core/src/test/java/com/ask/springcore/NamedParameterJdbcTemplateTest.java
+++ spring-core/src/test/java/com/ask/springcore/NamedParameterJdbcTemplateTest.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+/**
+ * @see <a href="https://www.baeldung.com/spring-jdbc-jdbctemplate">baeldung Spring JDBC</a>
+ */
 @SpringBootTest
 class NamedParameterJdbcTemplateTest {
 
