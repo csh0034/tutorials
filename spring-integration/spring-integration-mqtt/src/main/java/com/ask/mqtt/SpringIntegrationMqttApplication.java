42d9f7e84e94645c5769a568574dc2265dfd314d
csh0034 <csh0034@gmail.com>
2023-03-21 18:12:57 +0900

    feat(spring-integration-mqtt): init

---
+++ spring-integration/spring-integration-mqtt/src/main/java/com/ask/mqtt/SpringIntegrationMqttApplication.java
@@ -0,0 +1,13 @@
+package com.ask.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationMqttApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringIntegrationMqttApplication.class, args);
+  }
+
+}
