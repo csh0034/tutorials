7ffdc7f0cc20483df6f46041e30addc1d67b3385
csh0034 <csh0034@gmail.com>
2023-05-15 14:17:01 +0900

    test(java-core): stream, rank

--- java-core/src/test/java/com/ask/javacore/stream/StreamTest.java
+++ java-core/src/test/java/com/ask/javacore/stream/StreamTest.java
@@ -1,22 +1,35 @@
 package com.ask.javacore.stream;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class StreamTest {
 
   @Test
   void groupingBy() {
-    List<Integer> list = Arrays.asList(1, 2, 2, 3, 3, 3, 10);
+    List<Rank> ranks = Arrays.asList(Rank.FIRST, Rank.FIRST, Rank.THIRD);
 
-    Map<Integer, Long> result = list.stream()
+    Map<Rank, Long> result = ranks.stream()
         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
     System.out.println("result = " + result);
+
+    Map<Rank, Long> result2 = Stream.of(Rank.values())
+        .collect(toMap(Function.identity(), rank -> result.getOrDefault(rank, 0L), (a, b) -> b, LinkedHashMap::new));
+
+    System.out.println("result2 = " + result2);
+  }
+
+  enum Rank {
+    FIRST, SECOND, THIRD, FORTH
   }
 
 }
