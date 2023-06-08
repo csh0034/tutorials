bdd6952741b00b603809b217ff4f4932c4016deb
csh0034 <csh0034@gmail.com>
2023-06-08 11:19:12 +0900

    test(java-core): LocalDateTimeTest

--- java-core/src/test/java/com/ask/javacore/date/LocalDateTimeTest.java
+++ java-core/src/test/java/com/ask/javacore/date/LocalDateTimeTest.java
@@ -3,6 +3,8 @@ package com.ask.javacore.date;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
 import org.junit.jupiter.api.Test;
 
 class LocalDateTimeTest {
@@ -13,6 +15,16 @@ class LocalDateTimeTest {
     Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
     System.out.println(instant);
     System.out.println(instant.toEpochMilli());
+  }
+
+  @Test
+  void ofInstant() {
+    Instant twitterEpoch = Instant.ofEpochMilli(1288834974657L);
+    LocalDateTime localDateTime = LocalDateTime.ofInstant(twitterEpoch, ZoneOffset.UTC);
+    System.out.println(localDateTime);
+
+    Date date = Date.from(twitterEpoch);
+    System.out.println(date);
   }
 
 }
