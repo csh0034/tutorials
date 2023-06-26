9beb154ef3aaeb439d5eba02113d8840e81c817a
csh0034 <csh0034@gmail.com>
2023-06-26 13:17:35 +0900

    feat(spring-websocket): init

---
+++ spring-websocket/src/test/java/com/ask/websocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
