7de136a7ad56b6b3573126240144f2d143cf8303
csh0034 <csh0034@gmail.com>
2023-08-28 15:44:09 +0900

    feat(spring-integration-redis): init

---
+++ spring-integration/spring-integration-redis/README.md
@@ -0,0 +1,5 @@
+# Spring Integration Redis
+
+## 참조
+
+- [docs, spring integration redis support](https://docs.spring.io/spring-integration/docs/5.5.x/reference/html/redis.html#redis)
---
+++ spring-integration/spring-integration-redis/src/test/java/com/ask/integration/SpringIntegrationRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ask.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
